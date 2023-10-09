e1b91054e32a5463f8203c20f99b47dd6b188217
Boateng Paul Okeoghene <opaul816@gmail.com>
2023-10-09 17:10:20 +0000

    change the height of profilePhoto from full to max-content

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
           <Header />
         </div>
         <div className=" gap-4 h-full flex-1  flex flex-col sm:flex-row">
-          <div className=" bg-slate-200 rounded-2xl  sm:w-1/2 md:w-1/3">
+          <div className=" h-max bg-slate-200 rounded-2xl  sm:w-1/2 md:w-1/3">
             <ProfilePhoto />
           </div>
           <div className="flex-1 flex flex-col bg-slate-200 rounded-2xl sm:1/2 md:2/3">
